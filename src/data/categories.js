7e35f5fa0551a42532cdb17144dcfdce2a8cb3b4
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-04-04 18:37:18 +0530

    added categories data

---
+++ src/data/categories.js
@@ -0,0 +1,46 @@
+export const categories = [{
+        id: 1,
+        title: "General knowledge",
+        difficulty: "easy",
+    },
+    {
+        id: 2,
+        title: "Sports",
+        difficulty: "easy",
+    },
+    {
+        id: 3,
+        title: "Vehicles",
+        difficulty: "easy",
+    },
+    {
+        id: 4,
+        title: "History",
+        difficulty: "medium",
+    },
+    {
+        id: 5,
+        title: "Mythology",
+        difficulty: "medium",
+    },
+    {
+        id: 6,
+        title: "Politics",
+        difficulty: "medium",
+    },
+    {
+        id: 7,
+        title: "Computers",
+        difficulty: "hard",
+    },
+    {
+        id: 8,
+        title: "Books",
+        difficulty: "hard",
+    },
+    {
+        id: 9,
+        title: "Celebrities",
+        difficulty: "hard",
+    }
+]
