e556eb489dcb5d51ce0ff61b4427f9f0b58da961
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-04-05 21:05:00 +0530

    added category cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import { Homepage } from "./homepage/Homepage";
 import { CategoriesPage } from "./categories/CategoriesPage";
+import { CategoryCard } from "./categories/components/CategoryCard"
 
-export { Homepage, CategoriesPage };
+export { Homepage, CategoriesPage, CategoryCard };
