ebb16c7ddde3c554f85796991ea64867be6c346a
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-04-06 18:17:48 +0530

    added rules and image

--- src/pages/rulespage/RulesPage.js
+++ src/pages/rulespage/RulesPage.js
@@ -1,10 +1,18 @@
 import "./RulesPage.css";
+import "../../data/rules"
+import { rules } from "../../data/rules";
 
 function RulesPage(){
 
     return(
-        <div>
-            <h1>Rules</h1>
+        <div className="rules-container">
+            <div className="rules">
+                <h1>Rules</h1>
+                <ul>
+                    {rules.map(item => <li className="rule">{item.title}</li>)}
+                </ul>
+            </div>
+            <img src="/assets/images/rules-image.svg" alt="rules" />
         </div>
     );
 }
