bdbbbf805681f3380d50b5adf3755d77620415c2
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-04-07 21:18:15 +0530

    refactored results page

--- src/pages/results/Results.js
+++ src/pages/results/Results.js
@@ -1,15 +1,25 @@
-import { useQuestions } from "../../contexts";
+
 import "./Results.css";
 import {Link} from "react-router-dom";
+import {useQuestions} from "../../contexts";
 
 function Results(){
 
-    const {score} = useQuestions();
+    const {score,questions} = useQuestions();
+
 
     return(
         <div className="results">
             <h1>You have scored : {score} / 100</h1>
             <h3>You can explore more quizzes <Link to="/categories"> here...</Link></h3>
+            {
+                questions.map(item =>
+                    <div className="question">
+                        <h3>{item.question}</h3>
+                        <h2>Correct answer : {item.correct_answer}</h2>
+                    </div>
+                )
+            }
         </div>
     )
 }
